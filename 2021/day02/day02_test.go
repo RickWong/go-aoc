706c1729ddf9449a1ee39dfaa026ad166ae0cff0
Rick Wong <rick@newbuilds.nl>
2023-11-15 12:35:22 +0100

    Solve day 2.

---
+++ 2021/day02/day02_test.go
@@ -0,0 +1,95 @@
+package day02
+
+import (
+	_ "embed"
+	"fmt"
+	"strings"
+	"testing"
+)
+
+//go:embed example.txt
+var example string
+
+//go:embed input.txt
+var input string
+
+var data = input
+
+func part1() int {
+	lines := strings.Split(data, "\n")
+	x := 0
+	y := 0
+
+	for _, line := range lines {
+		var direction string
+		var steps int
+		fmt.Sscanf(line, "%s %d", &direction, &steps)
+
+		switch direction {
+		case "forward":
+			x += steps
+		case "down":
+			y += steps
+		case "up":
+			y -= steps
+		}
+	}
+	return x * y
+}
+
+func TestPart1(t *testing.T) {
+	result := part1()
+	expect := 15 * 10
+	if data == input {
+		expect = 2065 * 917 // 1_893_605
+	}
+
+	if result != expect {
+		t.Errorf("Result was incorrect, got: %d, expect: %d.", result, expect)
+	}
+}
+
+func BenchmarkPart1(b *testing.B) {
+	part1()
+}
+
+func part2() int {
+	lines := strings.Split(data, "\n")
+	x := 0
+	y := 0
+	aim := 0
+
+	for _, line := range lines {
+		var direction string
+		var steps int
+		fmt.Sscanf(line, "%s %d", &direction, &steps)
+
+		switch direction {
+		case "forward":
+			x += steps
+			y += steps * aim
+		case "down":
+			aim += steps
+		case "up":
+			aim -= steps
+		}
+	}
+	return x * y
+}
+
+func TestPart2(t *testing.T) {
+	result := part2()
+	expect := 15 * 60
+	if data == input {
+		expect = 2_120_734_350
+	}
+
+	if result != expect {
+		t.Errorf("Result was incorrect, got: %d, expect: %d.", result, expect)
+	}
+
+}
+
+func BenchmarkPart2(b *testing.B) {
+	part2()
+}
