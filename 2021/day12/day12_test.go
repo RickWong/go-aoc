f364e6070de2ff24158e734c3f3281167f064699
Rick Wong <rick@newbuilds.nl>
2023-11-20 12:21:47 +0100

    Implement configurable BFS/DFS/A* iterative search function. Solve day 12.

--- 2021/day12/day12_test.go
+++ 2021/day12/day12_test.go
@@ -2,7 +2,11 @@ package day12
 
 import (
 	_ "embed"
+	utils "github.com/RickWong/go-aoc/2021"
+	"maps"
+	"strings"
 	"testing"
+	"unicode"
 )
 
 //go:embed example.txt
@@ -11,17 +15,97 @@ var Example string
 //go:embed input.txt
 var Input string
 
-var data = Example
+var data = Input
+
+type Cave struct {
+	id      string
+	tunnels []string
+}
+
+type Route struct {
+	cave    *Cave
+	visited map[string]int
+	twice   bool
+}
 
 func part1() int {
-	return 0
+	lines := strings.Split(data, "\n")
+	caves := parseCaves(lines)
+	ends := 0
+
+	utils.IterativeSearch(
+		&Route{caves["start"], map[string]int{"start": 1}, false},
+		func(current *Route) []*Route {
+			var nextCaves []*Route
+			for _, id := range current.cave.tunnels {
+				if id == "start" {
+					continue
+				}
+
+				if id == "end" {
+					ends++
+					continue
+				}
+
+				if unicode.IsUpper(([]rune(id))[0]) ||
+					current.visited[id] < 1 {
+					visited := make(map[string]int, len(current.visited))
+					maps.Copy(visited, current.visited)
+					visited[id] = visited[id] + 1
+
+					nextCaves = append(
+						nextCaves,
+						&Route{caves[id], visited, false},
+					)
+				}
+			}
+			return nextCaves
+		},
+		func(current *Route) bool { return false },
+		nil,
+		nil,
+		nil,
+		0,
+	)
+
+	return ends
+}
+
+func parseCaves(lines []string) map[string]*Cave {
+	if lines == nil {
+		panic("No data")
+	}
+
+	caves := make(map[string]*Cave)
+
+	for _, line := range lines {
+		path := strings.Split(line, "-")
+		if path == nil {
+			panic("Invalid")
+		}
+
+		id := path[0]
+		nextCave := path[1]
+
+		if caves[id] == nil {
+			caves[id] = &Cave{id, nil}
+		}
+		caves[id].tunnels = append(caves[id].tunnels, nextCave)
+
+		if caves[nextCave] == nil {
+			caves[nextCave] = &Cave{nextCave, nil}
+		}
+		caves[nextCave].tunnels = append(caves[nextCave].tunnels, id)
+
+	}
+	return caves
 }
 
 func TestPart1(t *testing.T) {
 	result := part1()
-	expect := 1656
+	expect := 226
 	if data == Input {
-		expect = 1723
+		expect = 4338
 	}
 
 	if result != expect {
@@ -30,14 +114,57 @@ func TestPart1(t *testing.T) {
 }
 
 func part2() int {
-	return 0
+	lines := strings.Split(data, "\n")
+	caves := parseCaves(lines)
+	ends := 0
+
+	utils.IterativeSearch(
+		&Route{caves["start"], map[string]int{"start": 1}, false},
+		func(current *Route) []*Route {
+			var nextCaves []*Route
+			for _, id := range current.cave.tunnels {
+				if id == "start" {
+					continue
+				}
+
+				if id == "end" {
+					ends++
+					continue
+				}
+
+				isSmallCave := unicode.IsLower(([]rune(id))[0])
+
+				if !isSmallCave ||
+					(current.visited[id] == 0) ||
+					(current.visited[id] == 1 && !current.twice) {
+					visited := make(map[string]int, len(current.visited))
+					maps.Copy(visited, current.visited)
+					visited[id]++
+					twice := current.twice || (isSmallCave && visited[id] == 2)
+
+					nextCaves = append(
+						nextCaves,
+						&Route{caves[id], visited, twice},
+					)
+				}
+			}
+			return nextCaves
+		},
+		func(current *Route) bool { return false },
+		nil,
+		nil,
+		nil,
+		0,
+	)
+
+	return ends
 }
 
 func TestPart2(t *testing.T) {
 	result := part2()
-	expect := 195
+	expect := 3509
 	if data == Input {
-		expect = 327
+		expect = 114189
 	}
 
 	if result != expect {
---
+++ 2021/utils.go
@@ -0,0 +1,124 @@
+package utils
+
+import (
+	heap2 "github.com/zyedidia/generic/heap"
+	"math"
+	"slices"
+	"time"
+)
+
+type SearchResult[T any] struct {
+	Best         *T
+	Path         []*T
+	Milliseconds int64
+	Iterations   int
+}
+
+type heapItem[T any] struct {
+	priority float64
+	weight   float64
+	branch   *T
+}
+
+func IterativeSearch[T any](
+	start *T,
+	// branches are possible iterations based on the current.
+	branchFn func(current *T) []*T,
+	// predicate terminates the search when true.
+	predicateFn func(current *T) bool,
+	// identity is a map key that represents the unique iteration.
+	identityFn func(current *T) any,
+	// weightFn is a additive/cumulative weight.
+	weightFn func(current *T) float64,
+	// heuristicFn is an absolute weight modifier.
+	heuristicFn func(current *T) float64,
+	// beam width limits search space on each iteration.
+	beamWidth int,
+) *SearchResult[T] {
+	result := &SearchResult[T]{nil, nil, 0, 0}
+	now := time.Now().UnixMilli()
+
+	lessFn := func(a heapItem[T], b heapItem[T]) bool { return a.priority < b.priority }
+	heap := heap2.New(lessFn)
+	beam := heap2.New(lessFn)
+
+	var bestBranch *T
+	bestWeight := math.Inf(1)
+
+	var trail = make(map[any]*T)
+	var weights = make(map[any]float64)
+
+	heap.Push(heapItem[T]{0, 0, start})
+	for heap.Size() > 0 {
+		result.Iterations++
+		current, _ := heap.Pop()
+
+		if current.weight >= bestWeight {
+			continue
+		}
+
+		if predicateFn(current.branch) {
+			bestBranch = current.branch
+			bestWeight = current.weight
+			break
+		}
+
+		for _, branch := range branchFn(current.branch) {
+			weight := current.weight
+			if weightFn != nil {
+				weight += weightFn(branch)
+			}
+
+			var id any
+			if identityFn != nil {
+				id = identityFn(branch)
+			}
+
+			if id != nil {
+				if weight < weights[id] {
+					continue
+				}
+
+				weights[id] = weight
+				trail[id] = current.branch
+			}
+
+			priority := weight
+			if heuristicFn != nil {
+				priority += heuristicFn(branch)
+			}
+
+			if beamWidth == 0 {
+				heap.Push(heapItem[T]{priority, weight, branch})
+				continue
+			}
+
+			if beam.Size() >= beamWidth {
+				beam.Pop()
+			}
+
+			beam.Push(heapItem[T]{priority, weight, branch})
+		}
+
+		for beam.Size() > 0 {
+			item, _ := beam.Pop()
+			heap.Push(item)
+		}
+	}
+
+	if bestBranch != nil {
+		result.Path = append(result.Path, bestBranch)
+		nextStep := bestBranch
+		for nextStep != nil {
+			nextStep = trail[identityFn(nextStep)]
+			result.Path = append(result.Path, nextStep)
+			if nextStep == start {
+				break
+			}
+		}
+		slices.Reverse(result.Path)
+	}
+
+	result.Milliseconds = time.Now().UnixMilli() - now
+	return result
+}
