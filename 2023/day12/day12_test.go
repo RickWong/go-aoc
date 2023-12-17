2a1e34a969e3e09958c3e0546a136904f4f096d7
Rick Wong <rick@newbuilds.nl>
2023-12-17 23:39:19 +0100

    Partially done 2023 day 12, only part 1.

---
+++ 2023/day12/day12_test.go
@@ -0,0 +1,155 @@
+package day12
+
+import (
+	_ "embed"
+	"github.com/RickWong/go-aoc/2021/common"
+	"github.com/stretchr/testify/assert"
+	"golang.org/x/sync/errgroup"
+	"strconv"
+	"strings"
+	"sync/atomic"
+	"testing"
+)
+
+//go:embed example.txt
+var Example string
+
+//go:embed input.txt
+var Input string
+
+var data = Input
+
+// Data types.
+
+// Helper functions.
+
+func Atoi(s string) int {
+	v, _ := strconv.Atoi(s)
+	return v
+}
+
+func Map[T, R any](collection []T, fn func(a T) R) []R {
+	m := make([]R, len(collection))
+	for i, v := range collection {
+		m[i] = fn(v)
+	}
+	return m
+}
+
+func validate(springs string, groups []int) bool {
+	curGroup := 0
+	curSize := 0
+
+	for index := range springs {
+		switch springs[index] {
+		case '?':
+			return false
+		case '#':
+			curSize++
+		}
+
+		if springs[index] == '.' || index == len(springs)-1 {
+			if curSize > 0 {
+				// Found more groups than expected. Or current length doesn't match.
+				if curGroup >= len(groups) || curSize != groups[curGroup] {
+					return false
+				}
+
+				// Reset to count the next group.
+				curSize = 0
+				curGroup++
+			}
+		}
+	}
+
+	return curGroup == len(groups)
+}
+
+// Part 1.
+
+func part1() int {
+	lines := strings.Split(data, "\n")
+	sum := int64(0)
+	eg := errgroup.Group{}
+	eg.SetLimit(64)
+
+	for _, line := range lines {
+		line := line
+
+		eg.Go(func() error {
+			row := strings.Split(line, " ")
+			springs, sizes_ := row[0], row[1]
+			sizes := Map(strings.Split(sizes_, ","), Atoi)
+
+			common.IterativeSearch(
+				&springs,
+				func(s *string) []*string {
+					index := strings.Index(*s, "?")
+					if index >= 0 {
+						branches := make([]*string, 0, 2)
+						variant1 := (*s)[:index] + "#" + (*s)[index+1:]
+						variant2 := (*s)[:index] + "." + (*s)[index+1:]
+						branches = append(branches, &variant1, &variant2)
+						return branches
+					} else if validate(*s, sizes) {
+						atomic.AddInt64(&sum, 1)
+					}
+					return nil
+				},
+				nil,
+				nil,
+				nil,
+				nil,
+				0,
+			)
+
+			return nil
+		})
+	}
+
+	err := eg.Wait()
+	if err != nil {
+		panic(err)
+	}
+
+	return int(sum)
+}
+
+func TestPart1(t *testing.T) {
+	t.Parallel()
+
+	result := part1()
+
+	if data == Example {
+		assert.Equal(t, 21, result, "Result was incorrect")
+	} else {
+		assert.Equal(t, 7718, result, "Result was incorrect")
+	}
+}
+
+// Part 2.
+
+func part2() int {
+	return 0
+}
+
+func TestPart2(t *testing.T) {
+	t.Parallel()
+
+	result := part2()
+
+	if data == Example {
+		assert.Equal(t, 82000210, result, "Result was incorrect")
+	} else {
+		assert.Equal(t, 357134560737, result, "Result was incorrect")
+	}
+}
+
+// Kaizen. Kaizen. Kaizen.
+
+func BenchmarkAll(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		part1()
+		part2()
+	}
+}
--- go.mod
+++ go.mod
@@ -15,8 +15,9 @@ require (
 	github.com/segmentio/fasthash v1.0.3 // indirect
 	github.com/stretchr/testify v1.8.4 // indirect
 	github.com/zyedidia/generic v1.2.1 // indirect
-	golang.org/x/exp v0.0.0-20220303212507-bbda1eaf7a17 // indirect
+	golang.org/x/exp v0.0.0-20230321023759-10a507213a29 // indirect
 	golang.org/x/sync v0.5.0 // indirect
 	golang.org/x/sys v0.14.0 // indirect
+	gonum.org/v1/gonum v0.14.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
