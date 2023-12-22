be3d6e0efded3d985659ca2e21b7b1f82e640fd0
Rick Wong <rick@newbuilds.nl>
2023-12-22 22:13:25 +0100

    Done 2023 day 18 part 1.

--- 2023/day18/day18_test.go
+++ 2023/day18/day18_test.go
@@ -3,6 +3,7 @@ package day18
 import (
 	_ "embed"
 	"github.com/stretchr/testify/assert"
+	"strings"
 	"testing"
 )
 
@@ -12,16 +13,65 @@ var Example string
 //go:embed input.txt
 var Input string
 
-var data = Example
+var data = Input
 
 // Data types.
+
+type Hole struct {
+	y, x  int
+	color string
+}
 
 // Helper functions.
 
 // Part 1.
 
 func part1() int {
-	return 0
+	lines := strings.Split(strings.TrimSpace(data), "\n")
+	deltas := map[byte][2]int{
+		'R': {0, 1},
+		'D': {1, 0},
+		'U': {-1, 0},
+		'L': {0, -1},
+	}
+
+	y, x := 0, 0
+	holes := []Hole{{y, x, ""}}
+
+	// Without using an actual grid, track the min and max x for each line.
+	maxY := 0
+	lineMins := make(map[int]int)
+	lineMaxs := make(map[int]int)
+	lineMins[0] = 0
+	lineMaxs[0] = 0
+
+	for _, line := range lines {
+		parts := strings.Fields(line)
+		direction, meters, color := parts[0][0], int(parts[1][0]-'0'), parts[2][1:len(parts[2])-1]
+
+		for i := 0; i < meters; i++ {
+			y += deltas[direction][0]
+			x += deltas[direction][1]
+
+			maxY = max(maxY, y)
+			// Go maps don't return nil. Sadly.
+			_, ok := lineMins[y]
+			if !ok {
+				lineMins[y] = 999999999
+			}
+			lineMins[y] = min(lineMins[y], x)
+			lineMaxs[y] = max(lineMaxs[y], x)
+
+			holes = append(holes, Hole{y, x, color})
+		}
+	}
+
+	sum := 0
+	for y := 0; y <= maxY; y++ {
+		sum += lineMaxs[y] - lineMins[y] + 1
+	}
+
+	return sum
 }
 
 func TestPart1(t *testing.T) {
@@ -32,7 +82,7 @@ func TestPart1(t *testing.T) {
 	if data == Example {
 		assert.Equal(t, 62, result, "Result was incorrect")
 	} else {
-		assert.Equal(t, 8098, result, "Result was incorrect")
+		assert.Equal(t, 1790, result, "Result was incorrect")
 	}
 }
 
