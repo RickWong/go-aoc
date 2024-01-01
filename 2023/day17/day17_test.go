1fdd07d4fe1c24dd9e3197a8d28fbe12a1c41142
Rick Wong <rick@newbuilds.nl>
2024-01-01 16:44:31 +0100

    Done 2023 day 17 part 2. Use same A* algorithm, but support larger steps (NSWE) in the identity integer, and implement minimum and maximum steps.

--- 2023/day17/day17_test.go
+++ 2023/day17/day17_test.go
@@ -76,8 +76,11 @@ func part1() int {
 		func(cur *Trail) float64 {
 			return float64(grid[cur.y][cur.x].loss)
 		},
-		nil,
-		0,
+		func(cur *Trail) float64 {
+			return float64(end.y - cur.y + end.x - cur.x)
+		},
+		3,
+		false,
 		false,
 	)
 
@@ -99,7 +102,72 @@ func TestPart1(t *testing.T) {
 // Part 2.
 
 func part2() int {
-	return 0
+	lines := strings.Split(strings.TrimSpace(data), "\n")
+	grid := lo.Map(lines, func(line string, y int) []Block {
+		return lo.Map(strings.Split(line, ""), func(c string, x int) Block {
+			return Block{y, x, int(c[0] - '0')}
+		})
+	})
+
+	start := Trail{}
+	end := &grid[len(grid)-1][len(grid[0])-1]
+	result := common.IterativeSearch(
+		&start,
+		func(cur *Trail) []*Trail {
+			branches := make([]*Trail, 0, 4)
+
+			northAllowed := cur.y > start.y && cur.N < 10 && cur.S == 0 &&
+				((cur.S|cur.W|cur.E) == 0 || (cur.S|cur.W|cur.E) >= 4)
+			southAllowed := cur.y < end.y && cur.S < 10 && cur.N == 0 &&
+				((cur.N|cur.W|cur.E) == 0 || (cur.N|cur.W|cur.E) >= 4)
+			westAllowed := cur.x > start.x && cur.W < 10 && cur.E == 0 &&
+				((cur.N|cur.S|cur.E) == 0 || (cur.N|cur.S|cur.E) >= 4)
+			eastAllowed := cur.x < end.x && cur.E < 10 && cur.W == 0 &&
+				((cur.N|cur.S|cur.W) == 0 || (cur.N|cur.S|cur.W) >= 4)
+
+			if northAllowed {
+				branches = append(branches,
+					&Trail{cur.y - 1, cur.x, cur.N + 1, 0, 0, 0})
+			}
+			if southAllowed {
+				branches = append(branches,
+					&Trail{cur.y + 1, cur.x, 0, 0, cur.S + 1, 0})
+			}
+			if westAllowed {
+				branches = append(branches,
+					&Trail{cur.y, cur.x - 1, 0, 0, 0, cur.W + 1})
+			}
+			if eastAllowed {
+				branches = append(branches,
+					&Trail{cur.y, cur.x + 1, 0, cur.E + 1, 0, 0})
+			}
+			return branches
+		},
+		func(cur *Trail) bool {
+			return cur.y == end.y && cur.x == end.x && (cur.N|cur.S|cur.W|cur.E) >= 4
+		},
+		func(cur *Trail) any {
+			// Encode trail state as a single 64 bit integer.
+			ret := int64(cur.y)
+			ret |= int64(cur.x) << 10
+			ret |= int64(cur.N) << 20
+			ret |= int64(cur.E) << 24
+			ret |= int64(cur.S) << 28
+			ret |= int64(cur.W) << 32
+			return ret
+		},
+		func(cur *Trail) float64 {
+			return float64(grid[cur.y][cur.x].loss)
+		},
+		func(cur *Trail) float64 {
+			return float64(end.y - cur.y + end.x - cur.x)
+		},
+		0,
+		false,
+		false,
+	)
+
+	return int(result.BestWeight)
 }
 
 func TestPart2(t *testing.T) {
@@ -108,9 +176,9 @@ func TestPart2(t *testing.T) {
 	result := part2()
 
 	if data == Example {
-		assert.Equal(t, 82000210, result, "Result was incorrect")
+		assert.Equal(t, 94, result, "Result was incorrect")
 	} else {
-		assert.Equal(t, 357134560737, result, "Result was incorrect")
+		assert.Equal(t, 1268, result, "Result was incorrect")
 	}
 }
 
