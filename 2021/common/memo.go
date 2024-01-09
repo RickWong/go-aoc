e9cdb6f54ae54af03e2550fe3e5c4759e28fd4c3
Rick Wong <rick@newbuilds.nl>
2024-01-09 19:17:17 +0100

    Allow passing options to Memo, including a pre-allocated Cache and a custom Hash function, useful for when tuning performance.

--- 2021/common/memo.go
+++ 2021/common/memo.go
@@ -4,62 +4,70 @@ import (
 	"fmt"
 )
 
+type MemoOptions[Out any] struct {
+	Cache map[string]Out
+	Hash  func(data any) string
+}
+
 // Memo is a generic memoization function that accepts 1 argument.
-func Memo[Input comparable, Output any](f func(a Input) Output) func(a Input) Output {
-	cache := make(map[string]Output)
-	return func(a Input) Output {
-		key := fmt.Sprintf("%v", a)
-		if v, ok := cache[key]; ok {
+func Memo[A, Out any](f func(a A) Out, opts *MemoOptions[Out]) func(a A) Out {
+	if opts == nil {
+		opts = &MemoOptions[Out]{}
+	}
+	if opts.Cache == nil {
+		opts.Cache = make(map[string]Out)
+	}
+	return func(a A) Out {
+		var key string
+		if opts.Hash != nil {
+			key = opts.Hash(a)
+		} else {
+			key = fmt.Sprint(a)
+		}
+		if v, ok := opts.Cache[key]; ok {
 			return v
 		}
-
 		v := f(a)
-		cache[key] = v
+		opts.Cache[key] = v
 		return v
 	}
 }
 
 // Memo2 is a generic memoization function that accepts 2 arguments.
-func Memo2[Input1, Input2 comparable, Output any](f func(a Input1, b Input2) Output) func(a Input1, b Input2) Output {
-	cache := make(map[string]Output)
-	return func(a Input1, b Input2) Output {
-		key := fmt.Sprintf("%v", []any{a, b})
-		if v, ok := cache[key]; ok {
-			return v
-		}
-
-		v := f(a, b)
-		cache[key] = v
-		return v
+func Memo2[A, B, Out any](f func(a A, b B) Out, opts *MemoOptions[Out]) func(a A, b B) Out {
+	memoF := Memo(func(in []any) Out {
+		a, _ := in[0].(A)
+		b, _ := in[1].(B)
+		return f(a, b)
+	}, opts)
+	return func(a A, b B) Out {
+		return memoF([]any{a, b})
 	}
 }
 
 // Memo3 is a generic memoization function that accepts 3 arguments.
-func Memo3[Input1, Input2, Input3 comparable, Output any](f func(a Input1, b Input2, c Input3) Output) func(a Input1, b Input2, c Input3) Output {
-	cache := make(map[string]Output)
-	return func(a Input1, b Input2, c Input3) Output {
-		key := fmt.Sprintf("%v", []any{a, b, c})
-		if v, ok := cache[key]; ok {
-			return v
-		}
-
-		v := f(a, b, c)
-		cache[key] = v
-		return v
+func Memo3[A, B, C, Out any](f func(a A, b B, c C) Out, opts *MemoOptions[Out]) func(a A, b B, c C) Out {
+	memoF := Memo(func(in []any) Out {
+		a, _ := in[0].(A)
+		b, _ := in[1].(B)
+		c, _ := in[2].(C)
+		return f(a, b, c)
+	}, opts)
+	return func(a A, b B, c C) Out {
+		return memoF([]any{a, b, c})
 	}
 }
 
 // Memo4 is a generic memoization function that accepts 4 arguments.
-func Memo4[Input1, Input2, Input3, Input4 comparable, Output any](f func(a Input1, b Input2, c Input3, d Input4) Output) func(a Input1, b Input2, c Input3, d Input4) Output {
-	cache := make(map[string]Output)
-	return func(a Input1, b Input2, c Input3, d Input4) Output {
-		key := fmt.Sprintf("%v", []any{a, b, c, d})
-		if v, ok := cache[key]; ok {
-			return v
-		}
-
-		v := f(a, b, c, d)
-		cache[key] = v
-		return v
+func Memo4[A, B, C, D, Out any](f func(a A, b B, c C, d D) Out, opts *MemoOptions[Out]) func(a A, b B, c C, d D) Out {
+	memoF := Memo(func(in []any) Out {
+		a, _ := in[0].(A)
+		b, _ := in[1].(B)
+		c, _ := in[2].(C)
+		d, _ := in[3].(D)
+		return f(a, b, c, d)
+	}, opts)
+	return func(a A, b B, c C, d D) Out {
+		return memoF([]any{a, b, c, d})
 	}
 }
--- 2021/common/memo_test.go
+++ 2021/common/memo_test.go
@@ -3,14 +3,15 @@ package common
 import (
 	"errors"
 	"github.com/stretchr/testify/assert"
+	"strconv"
 	"testing"
 )
 
 func TestMemo(t *testing.T) {
-	f1 := Memo(func(a int) int { return a })
-	f2 := Memo2(func(a float32, b float64) float64 { return float64(a) + b })
-	f3 := Memo3(func(a string, b int, c any) any { return c })
-	f4 := Memo4(func(a string, b int, c float32, d error) bool { return d.Error() != "" })
+	f1 := Memo(func(a int) int { return a }, nil)
+	f2 := Memo2(func(a float32, b float64) float64 { return float64(a) + b }, nil)
+	f3 := Memo3(func(a string, b int, c any) any { return c }, nil)
+	f4 := Memo4(func(a string, b int, c float32, d error) bool { return d.Error() != "" }, nil)
 
 	assert.Equal(t, 1, f1(1))
 	assert.Equal(t, 1, f1(1))
@@ -35,6 +36,11 @@ func TestFibionacci(t *testing.T) {
 			return n
 		}
 		return fibionacci(n-1) + fibionacci(n-2)
+	}, &MemoOptions[int]{
+		Cache: make(map[string]int),
+		Hash: func(data any) string {
+			return strconv.Itoa(data.(int))
+		},
 	})
 
 	assert.Equal(t, 1, fibionacci(1))
@@ -51,5 +57,8 @@ func TestFibionacci(t *testing.T) {
 	calls = 0
 	assert.Equal(t, 5, fibionacci(5))
 	assert.Equal(t, 1, calls)
+	calls = 0
+	assert.Equal(t, 832040, fibionacci(30))
+	assert.Equal(t, 25, calls)
 	calls = 0
 }
