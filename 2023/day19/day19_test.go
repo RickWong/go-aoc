43658b38646fde1bf6a28d5bc32faaaffbef793d
Rick Wong <rick@newbuilds.nl>
2024-01-13 10:46:31 +0100

    Solved 2023 day 19 part 2. Use series (low to high) to model input space. Cut input space for each '<' and '>' operator, continuing in both partial input spaces instead of dropping one. Keep cutting while summing the product of series (all combinations) that reach the 'A' workflow. So this solution is an example of (recursive) counting sizes, as opposed to brute-force computing every input unit individually.

--- 2023/day19/day19_test.go
+++ 2023/day19/day19_test.go
@@ -2,8 +2,9 @@ package day19
 
 import (
 	_ "embed"
-	. "github.com/RickWong/go-aoc/2021/common"
+	. "github.com/RickWong/go-aoc/common"
 	"github.com/stretchr/testify/assert"
+	"maps"
 	"regexp"
 	"strings"
 	"testing"
@@ -36,17 +37,22 @@ type Rating = map[byte]int
 
 // Helper functions.
 
-// Part 1.
+func parseRatings(blob string) []Rating {
+	ratingsRegex := regexp.MustCompile(`(?m)^{x=(\d+),m=(\d+),a=(\d+),s=(\d+)}$`)
+	ratings := make([]Rating, 0)
+	for _, match := range ratingsRegex.FindAllStringSubmatch(blob, -1) {
+		x, m, a, s := Atoi(match[1]), Atoi(match[2]), Atoi(match[3]), Atoi(match[4])
+		ratings = append(ratings, Rating{'x': x, 'm': m, 'a': a, 's': s})
+	}
+	return ratings
+}
 
-func part1() int {
-	blocks := strings.Split(strings.TrimSpace(data), "\n\n")
+func parseWorkflows(blob string) map[string]Workflow {
 	workflowsRegex := regexp.MustCompile(`(?m)^(\w+){((?:\w+[<>]\d+:\w+,?)+\w+)}$`)
 	ruleRegex := regexp.MustCompile(`(\w+)([<>])(\d+):(\w+)`)
-	ratingsRegex := regexp.MustCompile(`(?m)^{x=(\d+),m=(\d+),a=(\d+),s=(\d+)}$`)
 	workflows := make(map[string]Workflow)
-	ratings := make([]Rating, 0)
 
-	for _, match := range workflowsRegex.FindAllStringSubmatch(blocks[0], -1) {
+	for _, match := range workflowsRegex.FindAllStringSubmatch(blob, -1) {
 		name, rules := match[1], match[2]
 		workflow := Workflow{name, nil, ""}
 
@@ -64,11 +70,15 @@ func part1() int {
 
 		workflows[name] = workflow
 	}
+	return workflows
+}
 
-	for _, match := range ratingsRegex.FindAllStringSubmatch(blocks[1], -1) {
-		x, m, a, s := Atoi(match[1]), Atoi(match[2]), Atoi(match[3]), Atoi(match[4])
-		ratings = append(ratings, Rating{'x': x, 'm': m, 'a': a, 's': s})
-	}
+// Part 1.
+
+func part1() int {
+	blocks := strings.Split(strings.TrimSpace(data), "\n\n")
+	workflows := parseWorkflows(blocks[0])
+	ratings := parseRatings(blocks[1])
 
 	sum := 0
 	for _, rating := range ratings {
@@ -116,8 +126,85 @@ func TestPart1(t *testing.T) {
 
 // Part 2.
 
+type Series struct {
+	Low  int
+	High int
+}
+
+func (ser Series) Size() int {
+	return max(0, ser.High-ser.Low+1)
+}
+
+func (ser Series) Cut(n int, operator byte) (Series, Series) {
+	other := ser // Copy.
+	switch operator {
+	case '<':
+		ser.High = n - 1
+		other.Low = n
+	case '>':
+		ser.Low = n + 1
+		other.High = n
+	}
+	return ser, other
+}
+
+type Combination map[byte]Series
+
+func (com Combination) Count() int {
+	prod := 1
+	for _, r := range com {
+		prod *= r.Size()
+	}
+	return prod
+}
+
+func (com Combination) Copy() Combination {
+	other := Combination{}
+	maps.Copy(other, com)
+	return other
+}
+
+func (com Combination) Cut(k byte, n int, operator byte) Combination {
+	other := com.Copy()
+	com[k], other[k] = com[k].Cut(n, operator)
+	return other
+}
+
+func countAccepted(com Combination, workflow Workflow, workflows map[string]Workflow) int {
+	count := 0
+
+	for _, rule := range workflow.rules {
+		remaining := com.Cut(rule.attribute, rule.value, rule.comparator)
+		if rule.workflow == "A" {
+			count += com.Count()
+		} else {
+			count += countAccepted(com.Copy(), workflows[rule.workflow], workflows)
+		}
+		com = remaining
+	}
+
+	if workflow.fallback != "" && com.Count() > 0 {
+		if workflow.fallback == "A" {
+			count += com.Count()
+		} else {
+			count += countAccepted(com.Copy(), workflows[workflow.fallback], workflows)
+		}
+	}
+
+	return count
+}
+
 func part2() int {
-	return 0
+	blocks := strings.Split(strings.TrimSpace(data), "\n\n")
+	workflows := parseWorkflows(blocks[0])
+	root := Combination{
+		'x': Series{1, 4000},
+		'm': Series{1, 4000},
+		'a': Series{1, 4000},
+		's': Series{1, 4000},
+	}
+
+	return countAccepted(root, workflows["in"], workflows)
 }
 
 func TestPart2(t *testing.T) {
@@ -126,9 +213,9 @@ func TestPart2(t *testing.T) {
 	result := part2()
 
 	if data == Example {
-		assert.Equal(t, 82000210, result)
+		assert.Equal(t, 167409079868000, result)
 	} else {
-		assert.Equal(t, 357134560737, result)
+		assert.Equal(t, 167409079868001, result)
 	}
 }
 
